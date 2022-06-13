fae1b15f2f9750d2c278b0cf5df77e76287035aa
Lukasz Wojcik <lukasz.wojcik.22@gmail.com>
2022-06-13 21:48:58 +0200

    Add products and basket

--- src/App.js
+++ src/App.js
@@ -6,21 +6,28 @@ import {LoginSuccessful} from "./components/LoginSuccessful";
 import {LoginContextProvider} from "./contexts/LoginContext";
 import {LoginPage} from "./components/LoginPage";
 import {LogoutPage} from "./components/LogoutPage";
+import {ShopContextProvider} from "./contexts/ShopContext";
+import {Products} from "./components/Products";
+import {Basket} from "./components/Basket";
 
 function App() {
   return (
       <LoginContextProvider>
-          <BrowserRouter>
-            <Routes>
-              <Route path='/' element={<Layout/>}>
-                  <Route index element={<MainPage/>}/>
-                  <Route path='login' element={<LoginPage/>}/>
-                  <Route path='logout' element={<LogoutPage/>}/>
-                  <Route path='login/google/:token&:email' element={<LoginSuccessful/>}/>
-                  <Route path='login/github/:token&:email' element={<LoginSuccessful/>}/>
-              </Route>
-            </Routes>
-          </BrowserRouter>
+          <ShopContextProvider>
+              <BrowserRouter>
+                <Routes>
+                  <Route path='/' element={<Layout/>}>
+                      <Route index element={<MainPage/>}/>
+                      <Route path='login' element={<LoginPage/>}/>
+                      <Route path='logout' element={<LogoutPage/>}/>
+                      <Route path='products' element={<Products/>}/>
+                      <Route path='basket' element={<Basket/>}/>
+                      <Route path='login/google/:token&:email' element={<LoginSuccessful/>}/>
+                      <Route path='login/github/:token&:email' element={<LoginSuccessful/>}/>
+                  </Route>
+                </Routes>
+              </BrowserRouter>
+          </ShopContextProvider>
       </LoginContextProvider>
   )
 }
